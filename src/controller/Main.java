009e587c5cac0f91c7665d17a882225743784334
kevin fuccio <kevin.fuccio00@gmai.com>
2023-04-03 16:05:46 +0200

    imp

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,7 +1,11 @@
 package controller;
 
-public class Main {
+import javax.persistence.EntityManager;
 
+import utils.JpaUtil;
+
+public class Main {
+	static  EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
 	public static void main(String[] args) {
 		
 	}
